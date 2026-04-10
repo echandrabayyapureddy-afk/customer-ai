de6ce657b62716154135de42ec18a0c03576e44f
echandrabayyapureddy-afk <e.chandrabayyapureddy@gmail.com>
2026-04-10 10:20:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 # ✅ Use stable model
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
+model = genai.GenerativeModel("gemini-1.0-pro")
 
 st.set_page_config(page_title="Customer AI", page_icon="🤖")
 st.title("🤖 Customer Frustration Intelligence Engine")
